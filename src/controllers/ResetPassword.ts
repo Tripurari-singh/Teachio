42279a94cd9858f9f4ff43fa95c7849a23feeaa7
Tripurari-singh <tripurari5821@gmail.com>
2025-09-02 15:09:12 +0530

    Reset_Password

--- src/controllers/ResetPassword.ts
+++ src/controllers/ResetPassword.ts
@@ -3,8 +3,24 @@ import { mailSender } from "../utils/MailSender";
 
 export const resetPasswordExpires = async (req : Request , res : Response) => {
     // Get email
+    const email = req.body;
     // Email validation
+    const user = await UserModel.findOne({email})
     // Generate Token
-    // update User by Adding Token
+    const token = crypto.randomUUID;
+    // Update UserModel by adding token and expiration Time
+    const updatedDetails = await UserModel.findOneAndUpdate({email : email} , {
+        token : token,
+        resetPasswordExpires : Date.now() + 3*60*1000,
+    } , {
+        // It Actually Updates The Model
+        new : true
+    })
+    // Create URL
+    const url = `http://localhost:3000/update-password/${token}`;
+    // Send Mail Containing URL
+    
+    // REtuen Response 
+    
     
 }
