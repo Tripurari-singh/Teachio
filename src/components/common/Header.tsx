5e93632a67a2d84f7b37972a56020f78bff7a9ca
Tripurari-singh <tripurari5821@gmail.com>
2025-10-26 17:53:13 +0530

    Header Component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,9 @@
 
 export function HeaderComponent(){
     return(
-        <div>
-          <div className="">
-
+        <div className="flex items-center justify-center border-2 shadow-2xl">
+          <div className="flex w-11/12 max-w-maxContent items-center justify-between">
+              
           </div>
         </div>
     )
