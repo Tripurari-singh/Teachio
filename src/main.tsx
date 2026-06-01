aa3753a4c94fb72a952a0984565ab272cbe33366
Tripurari-singh <tripurari5821@gmail.com>
2026-06-01 21:04:00 +0530

    Update application bootstrap configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from './reducers'
-
-export const store = configureStore({
-  reducer : rootReducer,
-})
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
